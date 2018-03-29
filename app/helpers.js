f44c274e544c4933341c6d67676e92b26666e878
ozzyonfire <mattoskamp@gmail.com>
2018-03-29 16:25:45 -0400

    test

--- app/helpers.js
+++ app/helpers.js
@@ -843,21 +843,25 @@ function addItemProperties(data, item) {
     if (item.barcode != data.DataExtValue && data.DataExtValue != '') {
       item.barcode = data.DataExtValue;
       item.updated = true;
+      console.log('barcode updated');
     }
   } else if (data.DataExtName == 'Location') {
     if (item.location != data.DataExtValue) {
       item.location = data.DataExtValue;
       item.updated = true;
+      console.log('loaction updated');
     }
   } else if (data.DataExtName == 'Country' || data.DataExtName == 'C Origin') {
     if (item.countryOfOrigin != data.DataExtValue) {
       item.countryOfOrigin = data.DataExtValue.toUpperCase();
       item.updated = true;
+      console.log('Country updated');
     }
   } else if (data.DataExtName == 'HTC Code') {
     if (item.htcCode != data.DataExtValue) {
       item.htcCode = data.DataExtValue;
       item.updated = true;
+      console.log('HTC updated');
     }
   }
 }
