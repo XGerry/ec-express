7cebaffc06b3b425a1814e255de2f7dbfc5c6090
ozzyonfire <mattoskamp@gmail.com>
2018-04-16 16:09:19 -0400

    changed the slack message

--- app/helpers.js
+++ app/helpers.js
@@ -1060,15 +1060,16 @@ function getSlackOrderReport(report) {
   var successFields = [];
   var failFields = [];
 
-  report.success.forEach((order) => {
-    var infoURL = 'https://www.ecstasycrafts.' + (order.canadian ? 'ca' : 'com') + '/admin/order_details.asp?orderid=' + order.cartOrder.OrderID;
-    var field = {
-      title: order.name,
-      value: '<'+infoURL+'|'+order.orderId + '> ' + (order.message == undefined ? '' : order.message),
-      short: true
-    };
-    successFields.push(field);
-  });
+  // TMI
+  // report.success.forEach((order) => {
+  //   var infoURL = 'https://www.ecstasycrafts.' + (order.canadian ? 'ca' : 'com') + '/admin/order_details.asp?orderid=' + order.cartOrder.OrderID;
+  //   var field = {
+  //     title: order.name,
+  //     value: '<'+infoURL+'|'+order.orderId + '> ' + (order.message == undefined ? '' : order.message),
+  //     short: true
+  //   };
+  //   successFields.push(field);
+  // });
 
   report.fail.forEach((order) => {
     var infoURL = 'https://www.ecstasycrafts.' + (order.canadian ? 'ca' : 'com') + '/admin/order_details.asp?orderid=' + order.cartOrder.OrderID;
