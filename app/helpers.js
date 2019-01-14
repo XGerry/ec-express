b0245d4087f1a11289b0260ae4cd3941433c58fb
ozzyonfire <mattoskamp@gmail.com>
2019-01-14 16:22:32 -0500

    debugging inative flag from quickbooks

--- app/helpers.js
+++ app/helpers.js
@@ -1555,7 +1555,7 @@ function queryAllItems(qbws) {
 }
 
 function runInventory(qbws) {
-  return Item.find({}).limit(10).then(items => {
+  return Item.find({sku: '60110576'}).then(items => {
     var siteRq = getItemSiteInventory(items);
     qbws.addRequest(siteRq, updateItemSites, true);
     return Item.update({}, {$set: {updated: false}});
