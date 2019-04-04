d31fa367ee44a9ecb168e9fca9b8637596696128
ozzyonfire <mattoskamp@gmail.com>
2019-04-04 11:33:17 -0400

    inventory fixes

--- app/model/marketplace.js
+++ app/model/marketplace.js
@@ -44,6 +44,7 @@ marketplaceSchema.methods.getSKUInfos = async function() {
 
 marketplaceSchema.methods.saveItem = async function(item) {
 	let cart = this.getCart();
+	let item = await mongoose.model('Item').findOne({sku: item.sku});
 	let response;
 	if (!item.isOption) {
 		let cartItem = item.getCartItem();
