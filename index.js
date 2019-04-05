16b89a6efafe6a3393cedf18b9e75c2c86369a5e
ozzyonfire <mattoskamp@gmail.com>
2019-04-05 11:55:41 -0400

    making the application use multiple clusters

--- index.js
+++ index.js
@@ -7,53 +7,74 @@ const api = require('./app/api');
 const routes = require('./app/routes');
 const webhooks = require('./app/webhooks');
 const events = require('./app/events');
+const cluster = require('cluster');
+const MongoStore = require('connect-mongo')(session);
+const numOfCPUs = require('os').cpus().length;
 
-var app = express();
-app.locals.moment = require('moment');
-app.locals._ = require('lodash');
-app.set('view engine', 'pug');
-app.use(express.static('client'));
-app.use(express.json({limit: '150mb'}));
-app.use(express.urlencoded({extended: true, limit: '50mb'}));
+if (cluster.isMaster) {
+  masterProcess();
+} else {
+  childProcess();
+}
 
-// Session setup
-let sessionMiddleware = session({
-  secret: 'an apple a day',
-  cookie: {
-    maxAge: 7 * 60 * 60 * 1000
-  },
-  resave: false,
-  saveUninitialized: true
-});
-app.use(sessionMiddleware);
+function masterProcess() {
+  console.log('Master ' + process.pid + ' is running.');
+  for (let i = 0; i < numOfCPUs; i++) {
+    console.log('Forking process number ' + i);
+    cluster.fork();
+  }
+}
 
-// prepare DB
-var uriString = process.env.MONGODB_URI || 
-                process.env.MONGOHQ_URL ||
-                'mongodb://localhost/db';
+function childProcess() {
+  console.log('Worker ' + process.pid + ' started...');
+  let app = express();
+  app.locals.moment = require('moment');
+  app.locals._ = require('lodash');
+  app.set('view engine', 'pug');
+  app.use(express.static('client'));
+  app.use(express.json({limit: '150mb'}));
+  app.use(express.urlencoded({extended: true, limit: '50mb'}));
 
-mongoose.connect(uriString, { useNewUrlParser: true }).then(() => {
-  console.log('Connected to database');
-}).catch(err => {
-  console.log('Error connecting to database');
-});
+  // prepare DB
+  let uriString = process.env.MONGODB_URI || 
+                  process.env.MONGOHQ_URL ||
+                  'mongodb://localhost/db';
 
-// TODO: clean up this
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/js', express.static(__dirname + '/node_modules/papaparse')); // redirect Papaparse
-app.use('/js', express.static(__dirname + '/node_modules/moment')); // redirect Moment
-app.use('/js', express.static(__dirname + '/node_modules/jsbarcode/dist')); // redirect JS Barcode
+  mongoose.connect(uriString, { useNewUrlParser: true }).then(() => {
+    console.log('Connected to database');
+  }).catch(err => {
+    console.log('Error connecting to database');
+  });
 
-var server = app.listen(process.env.PORT || 3000, function() {
-  console.log('EC-Express running on port 3000');
-  qbws.run(app);
-});
+  // Session setup
+  let sessionMiddleware = session({
+    store: new MongoStore({mongooseConnection: mongoose.connection}),
+    secret: 'an apple a day',
+    cookie: {
+      maxAge: 7 * 60 * 60 * 1000
+    },
+    resave: false,
+    saveUninitialized: true
+  });
+  app.use(sessionMiddleware);
 
-// socket.io
-var io = require('socket.io')(server);
-api.setQBWS(qbws);
-events(io, qbws);
-app.use('/api', api.router);
-routes(app);
-webhooks.route(app, qbws);
-require('./app/schedule')(qbws);
+  // TODO: clean up this
+  app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+  app.use('/js', express.static(__dirname + '/node_modules/papaparse')); // redirect Papaparse
+  app.use('/js', express.static(__dirname + '/node_modules/moment')); // redirect Moment
+  app.use('/js', express.static(__dirname + '/node_modules/jsbarcode/dist')); // redirect JS Barcode
+
+  let server = app.listen(process.env.PORT || 3000, function() {
+    console.log('EC-Express running on port 3000');
+    qbws.run(app);
+  });
+
+  // socket.io
+  var io = require('socket.io')(server);
+  api.setQBWS(qbws);
+  events(io, qbws);
+  app.use('/api', api.router);
+  routes(app);
+  webhooks.route(app, qbws);
+  require('./app/schedule')(qbws);
+}
